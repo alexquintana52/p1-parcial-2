beecfa9b9bd0f477d3c6d7dd77cef9a67443c49a
Alex Quintana <94649578+alexquintana52@users.noreply.github.com>
2022-12-04 19:47:16 -0300

    html layout, javascript buttons, products

---
+++ js/productos.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/*
+ *  Quintana Alex
+ */
+
+let productos = [
+    {
+        id: 1,
+        nombre: 'Procesador Intel Core i5-10400 BX8070110400 de 6 núcleos',
+        descripcion: 'Uno de los mejores procesadores para armar la pc gamer de tus sueños',
+        precio: 35.000,
+        imagen: './img/procesador.webp',
+        categoría: 'Procesadores',
+    },
+    {
+        id: 2,
+        nombre: 'Placa Madre Asus A320M-K Am4 DDR4 Usb 3.0 Hdmi A320',
+        descripcion: 'La mejor placa base económica para todos los bolsillos',
+        precio: 10.000,
+        imagen: './img/placa-madre.webp',
+        categoría: 'Placas Madre',
+    },
+    {
+        id: 3,
+        nombre: 'Memoria RAM Corsair Vengeance RGB Pro 8GBx2 ',
+        descripcion: 'Dos módulos de pura potencia para realizar tareas múltiples',
+        precio: 12.000,
+        imagen: './img/memoria-ram.webp',
+        categoría: 'Memorias RAM',
+    },
+    {
+        id: 4,
+        nombre: 'Fuente de Alimentacion Sentey Steel-Nitro SNP550-HS 550W',
+        descripcion: 'Fuente con watts reales para dar energía ',
+        precio: 8.000,
+        imagen: './img/fuente-alimentacion.webp',
+        categoría: 'Fuentes de Alimentacion',
+    },
+    {
+        id: 5,
+        nombre: 'Disco Estado Solido SSD Kingston 240GB',
+        descripcion: 'Descripción del producto',
+        precio: 3.000,
+        imagen: './img/disco-ssd.webp',
+        categoría: 'Discos',
+    },
+    {
+        id: 6,
+        nombre: 'Placa de Video MSI Ventus GeForce RTX 2060 8GB',
+        descripcion: 'Descripción del producto',
+        precio: 120.000,
+        imagen: './img/placa-grafica.webp',
+        categoría: 'Placas de Video',
+    },
+];
+
